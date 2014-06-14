766cf22c72dbaf92590649ba1c5f04da8ea0ac17
Diogo Peixoto <peixotoo@gmail.com>
2014-06-14 12:14:08 -0300

    Sequencia is working

---
+++ src/plp/programebrincando/expression/SequenciaExpressao.java
@@ -0,0 +1,17 @@
+package plp.programebrincando.expression;
+
+public class SequenciaExpressao implements Expressao{
+
+	public Expressao expressao1;
+	public Expressao expressao2;
+	
+	public SequenciaExpressao(Expressao expressao1, Expressao expressao2) {
+		this.expressao1 = expressao1;
+		this.expressao2 = expressao2;
+	}
+	
+	@Override
+	public String toString() {
+		return expressao1.toString() + " " + expressao2.toString();
+	}
+}
