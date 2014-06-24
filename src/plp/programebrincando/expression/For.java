8d86846e50fd1e203ec773006950f8dc01cf1117
Haroldo Gondim <hgtf@cin.ufpe.br>
2014-06-24 11:18:58 -0300

    Interpretador For OK \nFalta alguns TODO que tem dependência

--- src/plp/programebrincando/expression/For.java
+++ src/plp/programebrincando/expression/For.java
@@ -1,19 +1,24 @@
 package plp.programebrincando.expression;
 
+import com.sun.org.apache.bcel.internal.generic.INSTANCEOF;
+
 import plp.programebrincando.exception.VariavelJaDeclaradaException;
 import plp.programebrincando.exception.VariavelNaoDeclaradaException;
 import plp.programebrincando.expression.value.Valor;
+import plp.programebrincando.expression.value.ValorBooleano;
+import plp.programebrincando.expression.value.ValorInteiro;
 import plp.programebrincando.memory.AmbienteCompilacao;
 import plp.programebrincando.memory.AmbienteExecucao;
 import plp.programebrincando.util.Tipo;
+import plp.programebrincando.util.TipoPrimitivo;
 
 public class For implements Expressao {
 
-	public Expressao expressaoLoop;
+	public Expressao expressaoRepeticao;
 	public Expressao expressaoExecucao;
 
 	public For(Expressao expressaoLoop, Expressao expressaoExecucao) {
-		this.expressaoLoop = expressaoLoop;
+		this.expressaoRepeticao = expressaoLoop;
 		this.expressaoExecucao = expressaoExecucao;
 
 		System.out.println(toString());
@@ -21,27 +26,73 @@ public class For implements Expressao {
 
 	@Override
 	public String toString() {
-		return "for " + expressaoLoop.toString() + ", " + expressaoExecucao.toString();	
+		return "for " + expressaoRepeticao.toString() + ", " + expressaoExecucao.toString();	
 	}
 
 	@Override
-	public Valor avaliar(AmbienteExecucao amb)
-			throws VariavelNaoDeclaradaException, VariavelJaDeclaradaException {
-		// TODO Auto-generated method stub
+	public Valor avaliar(AmbienteExecucao amb) throws VariavelNaoDeclaradaException, VariavelJaDeclaradaException {
+		
+		Valor repeticao = expressaoRepeticao.avaliar(amb);
+		
+		if(repeticao instanceof ValorInteiro) {
+			
+			ValorInteiro valor = (ValorInteiro) repeticao;
+			
+			for(int i = 0; i < valor.valor(); i++) {
+				expressaoExecucao.avaliar(amb);
+			}
+			
+			
+		}
+	
+		/*
+		 * TODO Quando o resultado da expressão for um boleano
+		 * Porem só dá para testar essa parte quando aplicação estiver ok, pois váriáveis podem mudar de valor a cada iteração
+		 * Se não houver mudaça se torna um loop infinito
+		 * 
+
+		if(repeticao instanceof ValorBooleano) {
+			
+			ValorBooleano valor = (ValorBooleano) repeticao;
+
+			while(valor.valor()) {
+				expressaoExecucao.avaliar(amb);
+						
+				repeticao = expressaoRepeticao.avaliar(amb);
+				valor = (ValorBooleano) repeticao;				
+			}
+			
+		}
+		*/
+		
+		// TODO verificar o tipo de Retorno! Retornando null e está funcionando, só confirmar se deve ser isso mesmo ou se deve mudar
 		return null;
 	}
 
 	@Override
-	public boolean checaTipo(AmbienteCompilacao amb)
-			throws VariavelNaoDeclaradaException, VariavelJaDeclaradaException {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean checaTipo(AmbienteCompilacao amb) throws VariavelNaoDeclaradaException, VariavelJaDeclaradaException {
+		
+		
+		boolean retorno;
+		
+		/*
+		 * TODO Essa expressão será subistituida quando no avaliar já estiver sendo possível usar inteiros e boleanos
+		retorno = (expressaoRepeticao.checaTipo(amb)
+				&& (expressaoRepeticao.getTipo(amb).isBoolean()
+						|| expressaoRepeticao.getTipo(amb).isInteger())
+				&& expressaoExecucao.checaTipo(amb));
+		*/
+		
+		retorno = (expressaoRepeticao.checaTipo(amb)
+				&& expressaoRepeticao.getTipo(amb).isInteger()
+				&& expressaoExecucao.checaTipo(amb));
+		
+		return retorno;
 	}
 
 	@Override
-	public Tipo getTipo(AmbienteCompilacao amb)
-			throws VariavelNaoDeclaradaException, VariavelJaDeclaradaException {
-		// TODO Auto-generated method stub
-		return null;
+	public Tipo getTipo(AmbienteCompilacao amb) throws VariavelNaoDeclaradaException, VariavelJaDeclaradaException {
+		return TipoPrimitivo.BOOLEAN;
 	}
+
 }
